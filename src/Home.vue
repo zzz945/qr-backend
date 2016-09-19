762722daf697c5072112273ea15de1a786f14b23
zzz945 <642010820@qq.com>
2016-09-19 16:21:29 +0800

    添加msg列表

--- src/Home.vue
+++ src/Home.vue
@@ -22,6 +22,17 @@
         <div class="f-item">点餐</div>
       </flexbox-item>
     </flexbox>
+    <flexbox class="vux-1px-b" v-for="msg in msgList" v-on:click="showMsg(msg)">
+      <flexbox-item :span="1/3">
+        <div class="f-item">未读</div>
+      </flexbox-item>
+      <flexbox-item :span="1/3">
+        <div class="f-item">{{ msg.t_id + '号桌' }}</div>
+      </flexbox-item>
+      <flexbox-item :span="1/3">
+        <div class="f-item">{{msg.type}}</div>
+      </flexbox-item>
+    </flexbox>
   </div>
 </template>
 <script>
@@ -57,6 +68,8 @@ export default {
     return {
       myarray: myarray,
       orderList: [],
+      msgList: [],
+      rId: '',
       title: '没有新消息'
     }
   },
@@ -77,9 +90,24 @@ export default {
       })
       this.$route.router.go('/subpages/foodList')
     },
-    getOrders (_this, rId) {
+    showMsg (msg) {
+      console.log('showMsg idx=' + msg.idx)
+      console.log(msg)
       let params = {
-        r_id: rId
+        idx: msg.idx
+      }
+      let _this = this
+      _this.$http.get('http://tdkjgzh.applinzi.com/Home/Qrwaiter/msgIsRead', {params: params}).then((response) => {
+        console.log('msgIsRead:' + response.data)
+        msg.new = false
+      }, (response) => {
+        console.log('msgIsRead failed:')
+        console.log(response)
+      })
+    },
+    getOrders (_this) {
+      let params = {
+        r_id: this.rId
       }
 
       _this.$http.get('http://tdkjgzh.applinzi.com/Home/Qrorder/getNewOrder', {params: params}).then((response) => {
@@ -89,21 +117,21 @@ export default {
           return
         }
         let strList = JSON.parse(response.data)
-        let orderList = []
+        _this.orderList = []
         for (let i = 0; i < strList.length; i++) {
           let order = {}
           order.idx = strList[i].idx
           order.openid = strList[i].openid
           order.r_id = strList[i].r_id
           order.t_id = strList[i].t_id
+          order.is_new = strList[i].is_new
           order.food_list = []
           let foodList = JSON.parse(strList[i].food_list)
           for (let i = 0; i < foodList.length; i++) {
             order.food_list.push(JSON.parse(foodList[i]))
           }
-          orderList.push(order)
+          _this.orderList.push(order)
         }
-        _this.orderList = orderList
         _this.title = '有新消息了'
         console.log('_this.orderList:')
         console.log(_this.orderList)
@@ -111,18 +139,44 @@ export default {
         console.log('getNewOrder failed:')
         console.log(response)
       })
+
+      _this.$http.get('http://tdkjgzh.applinzi.com/Home/Qrwaiter/getNewMsgs', {params: params}).then((response) => {
+        console.log('getNewMsgs')
+        console.log(response.data)
+        if (response.data === '') {
+          return
+        }
+
+        let strList = JSON.parse(response.data)
+        _this.msgList = []
+        for (let i = 0; i < strList.length; i++) {
+          let msg = {}
+          msg.idx = strList[i].idx
+          msg.r_id = strList[i].r_id
+          msg.t_id = strList[i].t_id
+          msg.type = strList[i].type
+          msg.new = strList[i].new
+          _this.msgList.push(msg)
+        }
+
+        _this.title = '有新消息了'
+        console.log('_this.msgList:')
+        console.log(_this.msgList)
+      }, (response) => {
+        console.log('getNewMsgs failed:')
+        console.log(response)
+      })
     }
   },
   ready () {
     let openid = document.getElementById('arg_openid').innerHTML
-    let rId = ''
     for (let i = 0; i < this.myarray.length; i++) {
       if (openid === myarray[i].openid) {
-        rId = myarray[i].r_id
+        this.rId = myarray[i].r_id
       }
     }
 
-    if (rId === '') {
+    if (this.rId === '') {
       this.$vux.alert.show({
         title: '错误',
         content: '很抱歉，您不是已注册客服人员',
@@ -133,10 +187,10 @@ export default {
     }
 
     let _this = this
-    _this.getOrders(_this, rId)
+    _this.getOrders(_this)
     threadId = setInterval(function () {
-      _this.getOrders(_this, rId)
-    }, 10000)
+      _this.getOrders(_this)
+    }, 60000)
   }
 }
 </script>
